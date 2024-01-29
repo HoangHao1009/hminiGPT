9ac0dd25c109d06c3b39ce85c80ceab6e775429c
Hà Hoàng Hảo <112957852+HoangHao1009@users.noreply.github.com>
2024-01-29 08:12:28 +0700

    Update model.py

--- hGPT/model.py
+++ hGPT/model.py
@@ -1,138 +1,214 @@
 import torch
-import random
-import mmap
-import csv
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
-from torch.utils.data import Dataset
-import ast
 
-PAD_token = 0
-UNK_token = 1
-
-class DataCreator:
-    def __init__(self, block_size, tokenizer):
-        self.block_size = block_size
-        self.tokenizer = tokenizer
-        self.pairs = []
-    def extractPairs(self, file_path, n_pairs):
-        pairs = []
-        with open(file_path, 'rb') as f:
-            with mmap.mmap(f.fileno(), 0, access = mmap.ACCESS_READ) as mm:
-                file_size = len(mm)
-                for _ in tqdm(range(n_pairs), desc = 'Processing items', unit = 'item'):
-                    i = random.randint(0, file_size - self.block_size * 10)
-                    chunk = mm[i:i + self.block_size * 15].decode('utf-8', errors = 'ignore').replace('\r', '').lower()
-                    sent = self.tokenizer(chunk)
-                    sent = [str(x) for x in sent]
-                    sent = [i for i in sent if i != ' ' and i != None]
-                    start_pos = random.randint(0, len(sent) - self.block_size - 1)
-                    input = sent[start_pos: start_pos + self.block_size]
-                    target = sent[start_pos + 1: start_pos + self.block_size + 1]
-                    
-                    pairs.append([input, target])
-        self.pairs = pairs
-    def csvwrite(self, file_path, delimiter):
-        with open(file_path, 'w', encoding = 'utf-8') as outputfile:
-            writer = csv.writer(outputfile, delimiter = delimiter, lineterminator = '\n')
-            for pair in self.pairs:
-                writer.writerow(pair)
-
-    def csvread(self, file_path, delimiter):
-        with open(file_path, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter = delimiter, lineterminator = '\n')
-            results = []
-            for row in tqdm(csv_reader, desc = 'Reading CSV', unit = 'rows'):
-                input = ast.literal_eval(row[0])
-                target = ast.literal_eval(row[1])
-                results.append([input, target])
-        self.pairs = results
-
-class Vocabulary:
-    def __init__(self):
-        self.trimmed = False
-        self.word2index = {}
-        self.word2count = {}
-        self.index2word = {PAD_token: 'PAD', UNK_token: 'UNK'}
-        self.num_words = 2
-        self.vocab_size = self.num_words
-
-    def addSent(self, sentence):
-        for word in sentence:
-            self.addWord(word)
-
-    def addWord(self, word):
-        if word not in self.word2index:
-            self.word2index[word] = self.num_words
-            self.word2count[word] = 1
-            self.index2word[self.num_words] = word
-            self.num_words += 1
-        else:
-            self.word2count[word] += 1
-
-    def trim(self, min_count):
-        if self.trimmed:
-            return
-        self.trimmed = True
-
-        keep_words = []
-
-        for k, v in self.word2count.items():
-            if v >= min_count:
-                keep_words.append(k)
-        
-        print(f'Keep {len(keep_words)} in total {len(self.word2index)} = {len(keep_words) / len(self.word2index):2%}')
-
-        self.word2index = {}
-        self.word2count = {}
-        self.index2word = {PAD_token: 'PAD', UNK_token: 'UNK'}
-        self.num_words = 2
-
-        for word in keep_words:
-            self.addWord(word)
-
-        self.vocab_size = len(self)
-
-    def encode(self, li):
-        return [self.word2index.get(word, 1) for word in li]
-    
-    def decode(self, li):
-        return ' '.join([self.index2word[i] for i in li])
-    
-    def __len__(self):
-        return self.vocab_size
-
-class CustomDataset(Dataset):
-    def __init__(self, pairs, voc, device, min_count = 1, type = 'train'):
+class HeadAttention(nn.Module):
+    def __init__(self, n_emb, head_size, block_size, dropout):
         super().__init__()
-        self.pairs = pairs
-        self.voc = voc
-        self.min_count = min_count
-        self.type = type
-        self.device = device
-        self.X, self.y = self.get_data()
-        self.n_samples = self.X.shape[0]
+        self.key = nn.Linear(n_emb, head_size, bias = False)
+        self.query = nn.Linear(n_emb, head_size, bias = False)
+        self.value = nn.Linear(n_emb, head_size, bias = False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # C = n_emb
+        # x: (B, T, C)
+        B, T, C = x.shape
+        k = self.key(x) # (B, T, H) H: head_size
+        q = self.query(x) # (B, T, H)
+        v = self.value(x) # (B, T, H)
+
+        wei = q @ k.transpose(-2, -1) * k.shape[-1] ** 0.5
+        # -> Dot product similarity between each word's key with and each word's query
+        # wei: (B, T, H) @ (B, H, T) = (B, T, T)
+
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim = -1) # (B, T, T (probs))
+        # -> Probability of words in Time-steps by each given word
+        wei = self.dropout(wei) # (B, T, T)
+        out = wei @ v # -> Dot product similarity
+        # (B, T, T) @ (B, T, H) = (B, T, H)
+        # -> Attention vector of each words (head_size)
+        return out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_head, head_size, block_size, n_emb, dropout):
+        super().__init__()
+        self.heads = nn.ModuleList([HeadAttention(n_emb, head_size, block_size, dropout) for _ in range(num_head)])
+        self.proj = nn.Linear(head_size * num_head, n_emb)
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, x):
+        # x: (B, T, C)
+        out = torch.cat([h(x) for h in self.heads], dim = -1) # (B, T, H * num_head)
+        proj = self.proj(out) # (B, T, C) C = n_emb
+        out = self.dropout(proj) # (B, T, C)
+        return out
+
+class TransformerBlock(nn.Module):
+    """Transformer block"""
+    def __init__(self, num_head, block_size, n_emb, dropout):
+        super().__init__()
+        head_size = n_emb // num_head
+        self.self_attention = MultiHeadAttention(num_head, head_size, block_size, n_emb, dropout)
+        self.feedforward = nn.Sequential(
+            nn.Linear(n_emb, 4 * n_emb),
+            nn.ReLU(),
+            nn.Linear(4 * n_emb, n_emb),
+            nn.Dropout(dropout),
+        )
+        self.ln1 = nn.LayerNorm(n_emb)
+        self.ln2 = nn.LayerNorm(n_emb)
+    
+    def forward(self, x):
+        # x: (B, T, C)
+        y = self.self_attention(x) # (B, T, C)
+        x = self.ln1(x + y) # (B, T, C)
+        y = self.feedforward(x) # (B, T, C)
+        x = self.ln2(x + y) # (B, T, C)
+        return x
         
-    def get_data(self):
-        X_total = []
-        y_total = []
-        if self.type == 'train':
-            for input, target in self.pairs:
-                self.voc.addSent(input)
-                self.voc.addSent(target)
-            self.voc.trim(self.min_count)
-        for input, target in self.pairs: 
-            input = self.voc.encode(input)
-            target = self.voc.encode(target)
-            X_total.append(input)
-            y_total.append(target)
-        X_final = torch.tensor(X_total)
-        y_final = torch.tensor(y_total)
-        X_final = X_final.to(self.device)
-        y_final = y_final.to(self.device)
-        return X_final, y_final
+
+class MiniGPT(nn.Module):
+    """Mini GPT Model"""
+    def __init__(self, vocab_size, n_emb, block_size, num_head, n_layer, dropout, device):
+        super().__init__()
+        self.block_size = block_size
+        self.token_embedding = nn.Embedding(vocab_size, n_emb)
+        self.position_embedding = nn.Embedding(block_size, n_emb)
+        self.trans_blocks = nn.Sequential(*[TransformerBlock(num_head, block_size, n_emb, dropout) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_emb)
+        self.lm_head = nn.Linear(n_emb, vocab_size)
+        self.device = device
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean = 0.0, std = 0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean = 0.0, std = 0.02)
+        
+    def forward(self, index, targets = None):
+        # B -> batch_size, T -> block_size (Time-steps)
+        B, T = index.shape # index shape: (B, T)
+
+        tok_emb = self.token_embedding(index) # (B, T, C)
+        pos_emb = self.position_embedding(torch.arange(T, device = self.device)) # (T, C)
+
+        x = tok_emb + pos_emb # (B, T, C)
+        x = self.trans_blocks(x) # (B, T, C)
+        x = self.ln_f(x) # (B, T, C)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, vocab_size = logits.shape
+            logits = logits.view(B*T, vocab_size)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, index, max_new_tokens):
+        # index: (1, X) -> words in index
+        for _ in range(max_new_tokens):
+            index_cond = index[:, -self.block_size:] # (1, T)
+            logits, loss = self.forward(index_cond)
+            # logits: (1, T, vocab_size)
+
+            # focus only in the last time steps
+            logits = logits[: , -1, :] # (1, 1, vocab_size)
+
+            #get probs
+            probs = F.softmax(logits, dim = -1) # (1, 1, vocab_size (probs))
+
+            #sample
+            index_next = torch.multinomial(probs, num_samples = 1) # (1)
+
+            index = torch.cat((index, index_next), dim = 1) # ( + 1) -> till end loop
+        #index: (max_new_token, )
+        return index
     
-    def __getitem__(self, index):
-        return self.X[index], self.y[index]
+class Trainer(nn.Module):
+    def __init__(self, train_dataloader, val_dataloader, model, optimizer, eval_iters):
+        super().__init__()
+        self.model = model
+        self.train_data = train_dataloader
+        self.val_data = val_dataloader
+        self.optimizer = optimizer
+        self.eval_iters = eval_iters
     
-    def __len__(self):
-        return self.n_samples
+    @torch.no_grad()
+    def estimate_loss(self):
+        out = {}
+        self.model.eval()
+        for split in ['train', 'val']:
+            data = self.train_data if split == 'train' else self.val_data
+            losses = torch.zeros(self.eval_iters)
+            for k in range(self.eval_iters):
+                X, y = next(iter(data))
+                logits, loss = self.model(X, y)
+                losses[k] = loss.item()
+            out[split] = losses.mean()
+        self.model.train()
+        return out
+    
+    def start_training(self, train_iters):
+        for i in range(train_iters):
+            with torch.no_grad():
+                if i % 50 == 0:
+                    losses = self.estimate_loss()
+                    print(f"Step {i}: Train loss: {losses['train']}/ Val loss: {losses['val']}")
+            
+            xb, yb = next(iter(self.train_data))
+
+            logits, loss = self.model(xb, yb)
+            self.optimizer.zero_grad(set_to_none = True)
+            loss.backward()
+            self.optimizer.step()
+
+    def save_model(self, voc, file_path):
+        torch.save({
+            'model': self.model.state_dict(),
+            'opt': self.optimizer.state_dict(),
+            'voc': voc.__dict__
+        }, file_path)
+
+class WordVector:
+    def __init__(self, vocab, model, device):
+        self.vocab = vocab
+        self.model = model
+        word_index = [word for word in self.vocab.word2index.keys()]
+        word_vector = {}
+        for word in tqdm(word_index, desc = 'Computing Word Vectos', unit = 'word'):
+            idx = torch.tensor(self.vocab.word2index[word]).to(device)
+            word_vector[word] = model.token_embedding(idx)
+        self.word_vector = word_vector
+        for word, vector in self.word_vector.items():
+            self.word_vector[word] = vector.cpu().detach().numpy()
+
+    def similarity(self, w1, w2):
+        return cosine_similarity([self.word_vector[w1]], [self.word_vector[w2]])[0][0]
+    
+    def most_similarity(self, word, type = 'simi', top_n = 5):
+        try:
+            w_cs = {}
+            for w, v in self.word_vector.items():
+                if w != word:
+                    cs = cosine_similarity([v],[self.word_vector[word]])[0][0]
+                    w_cs[w] = cs
+            if type == 'simi':
+                sort_w_cs = sorted(w_cs.items(), key = lambda i: i[1], reverse = True)
+                results = sort_w_cs[:top_n]
+            elif type == 'differ':
+                sort_w_cs = sorted(w_cs.items(), key = lambda i: i[1])
+                results = sort_w_cs[:top_n]
+        except:
+            print('Type is "simi" or "differ')
+        return results
